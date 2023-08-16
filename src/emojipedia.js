b3bf3b6cacf52898f4ab1a39cdd52544bb7457f5
Geekytoocode <ashishkumarr1108@gmail.com>
2023-08-16 19:56:11 +0530

    First Commit

---
+++ src/emojipedia.js
@@ -0,0 +1,66 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "😆",
+    name: "Smiling Face With Open Mouth And Closed Eyes",
+    meaning:
+      " can't stop laughing! Triggered by incredibly funny or ridiculous situations, the grinning and contorted face emoji 😆 expresses the feeling of being on the verge of a fit of laughter"
+  },
+  {
+    id: 5,
+    emoji: "😄",
+    name: "smiling Face With Open Mouth And Smiling Eyes",
+    meaning:
+      " A smiling face with laughing eyes 😄 represents loud, boisterous and hearty laughter, with the typical laughing emoji eyes that distinguish a real smile from a fake one."
+  },
+  {
+    id: 6,
+    emoji: "😅",
+    name: "Smiling Face With Open Mouth & Cold Sweat",
+    meaning:
+      " A grinning face with drops of sweat is represented by the 😅 emoji. This is used to express relief that a tricky situation has been successfully handled or just gone well."
+  },
+  {
+    id: 7,
+    emoji: "😇",
+    name: "Smiling Face With Halo",
+    meaning:
+      " A heavenly smile is shown by the 😇 halo emoji, which symbolizes innocence and goodness."
+  },
+  {
+    id: 8,
+    emoji: "😊",
+    name: "Smiling Face With Smiling Eyes",
+    meaning:
+      "With a shy, embarrassed smile, the 😊 emoji radiates happiness, contentment, peace of mind, and gratitude."
+  },
+  {
+    id: 9,
+    emoji: "🙃",
+    name: "Upside-Down Face",
+    meaning:
+      "'m not serious! The inverted smiling face 🙃 indicates that you are not serious or have a humorous undertone. It is suitable for ambiguous statements, ironic remarks or joking comments."
+  }
+];
+export default emojipedia;
